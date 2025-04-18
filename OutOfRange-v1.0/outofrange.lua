746dd042377ccbcf15d4479a220e8995021a6c43
uduman <65922418+uduman@users.noreply.github.com>
2025-04-18 13:07:05 -0400

    keep unavailable actions greyed out

--- OutOfRange-v1.0/outofrange.lua
+++ OutOfRange-v1.0/outofrange.lua
@@ -4,15 +4,25 @@ local elapsed = 0
 local function UpdateButtonRange(self)
     local action = self.action
     local icon = self.icon
-    if action and IsActionInRange(action) == false then
-        icon:SetVertexColor(1.0, 0.1, 0.1)
-    else
-        icon:SetVertexColor(1.0, 1.0, 1.0)
+    if action then
+        local isUsable, notEnoughMana = IsUsableAction(action)
+        local inRange = IsActionInRange(action)
+        if not isUsable then
+            -- Action is unusable (talent not picked, no mana, etc.)
+            icon:SetVertexColor(0.4, 0.4, 0.4) -- grey (unusable)
+        elseif inRange == false then
+            -- Action is usable but out of range
+            icon:SetVertexColor(1.0, 0.1, 0.1) -- red (out of range)
+        else
+            -- Action is usable and in range
+            icon:SetVertexColor(1.0, 1.0, 1.0) -- normal color
+        end
     end
+
 end
 
-local f = CreateFrame("Frame")
-f:SetScript("OnUpdate", function(self, delta)
+local frame = CreateFrame("Frame")
+frame:SetScript("OnUpdate", function(self, delta)
     elapsed = elapsed + delta
     if elapsed >= UPDATE_INTERVAL then
         elapsed = 0
